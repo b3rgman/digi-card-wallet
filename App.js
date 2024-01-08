0b5946fc7020387c2e55adecf67e270171434771
Josh Bergman <josh.bergman@apiture.com>
2024-01-07 19:56:56 -0500

    updating Repo.

--- App.js
+++ App.js
@@ -4,22 +4,36 @@ import { StyleSheet, View, Image } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 
+import * as ImagePicker from 'expo-image-picker';
+
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+    return (
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} />
+        </View>
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" /* @info */ onPress={pickImageAsync} /* @end */ />
+          <Button label="Use this photo!!" />
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    )
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
